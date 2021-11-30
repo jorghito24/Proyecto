32f373261c3e54dbc68966a6c05cb6294272fe57
jorghito24 <jorghito2403@gmail.com>
2021-11-30 11:18:36 -0300

    proyecto java

---
+++ src/BD/Conexion.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BD;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author tu_jo
+ */
+public class Conexion {
+    
+    public Connection getConnection(){
+        Connection cn = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/videojuegos","root","");
+            System.out.println("Conexion exitosa");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error de Clase conexion" +e.getMessage());
+            
+            
+            
+        }
+        return cn;
+    }
+//    public static void main(String[] args) {
+//        Conexion cn = new Conexion();
+//        cn.getConnection();
+//  }
+}
